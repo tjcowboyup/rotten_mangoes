eb52bd4006419bef05ebdd126235e25db6eef46d
Tom Wang <15tomw@gmail.com>
2016-02-03 19:34:38 +0000

    routing root to movies#index

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -21,6 +21,10 @@ class Movie < ActiveRecord::Base
     presence: true
 
   validate :release_date_is_in_the_past
+  
+  def review_average
+  reviews.sum(:rating_out_of_ten)/reviews.size
+  end
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 RottenMangoes::Application.routes.draw do
+  get "reviews/new"
+  get "reviews/create"
+  get "new/create"
   get "sessions/new"
   get "sessions/create"
   get "users/new"
   get "users/create"
-  resources :movies
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 
